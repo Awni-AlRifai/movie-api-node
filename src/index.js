d7eb913bf0df10f1adbc8fe349523a9abd484154
Awni-Alrifai <Awni.Rifai@optimumpartners.co>
2022-04-25 17:44:26 +0300

    restrucure the files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 const express = require('express');
-const YAML = require('yamljs');
+const routes = require('./routes');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT;
 
-const swaggerUi = require('swagger-ui-express');
-const userRoutes = require('./routes/user');
-const movieRoutes = require('./routes/movie');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', routes);
 
-app.use('/users', userRoutes);
-app.use('/movies', movieRoutes);
-
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(YAML.load('swagger.yml'))
-);
 app.get('/', (request, response) => response.send('Welcome'));
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
